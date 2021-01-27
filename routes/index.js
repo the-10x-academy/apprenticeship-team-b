fc25119a65c9c87b3e5ea36843278ab643d873fc
vaishnavi <manchalavaishnavi07@gmail.com>
2021-01-27 15:11:31 +0530

    made changes in name from product to post

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-var Post=require('../models/product')
+var Post=require('../models/post')
 /* GET home page. */
 router.get("/postsapi", function (req, res, next) {
 	Post.find({}).then(model => {   // <-- Update to your call of choice.
