d810c6c8efd57649d65e6e143286780164af3dd7
vaishnavi <manchalavaishnavi07@gmail.com>
2021-01-29 10:19:15 +0530

    index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+var cors = require('cors')
 var Post=require('../models/post')
 /* GET home page. */
 router.get("/postsapi", function (req, res, next) {
