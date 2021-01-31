27aa2009774ec1928abae54ef9289075bdd8e2ad
Hemant <hemant.bhatt1997@gmail.com>
2021-01-31 12:28:26 +0530

    furnishing

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import icon from "./icon.png";
-
+import camera from "./camera.png"
 import "./header.css";
 
 
@@ -12,7 +12,7 @@ function Header(){
         <img src = {icon} id = "icon"></img>
         <text id = "logo">Instaclone</text>
         </div>
-        <button id = "camera"></button>
+        <button class = "buttons"><img src = {camera} id = "camera"></img></button>
     </div>
     );
 }
