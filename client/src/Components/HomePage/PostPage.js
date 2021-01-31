baa7ea7d46f59b6b08c0bee9eb13d5f2580bb58e
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-01-31 20:58:19 +0530

    added props and made dynamic

--- client/src/Components/HomePage/PostPage.js
+++ client/src/Components/HomePage/PostPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './HomePage.css';
-import bikes from './bikes.jpg'
 import heart from './heart.png'
 import share from './share.png'
 function Enter(props){
@@ -8,18 +7,24 @@ function Enter(props){
     return(
         <div className='postpage_all'>
         <div className='postpage_box'>
-            <h1 className='postpage_name'>name</h1>
-            <h1 className='postpage_location'>location</h1>
-            
-            <img src={bikes} alt='' className='postpage_image'/>
+            <div className='postpage_nameLoc'>
+                <h1 className='postpage_name'>{props.name}</h1>
+                <h1 className='postpage_location'>{props.location}</h1>
+            </div>           
+            <div className='postpage_bikediv' style={{ 
+      backgroundImage: `url(${props.imagepath})`}}>
+            </div>
+            <div className='postpage_heartshare'>
             <input type="image" src={heart} alt=' ' className='postpage_heart'/>
             <input type='image' src={share} alt=' ' className='postpage_share'/>
-            <h1 className='postpage_date'>10Jan 2021</h1>
-            
-            
-            <h1 className='postpage_likes'>0 likes</h1>
-            <h1 className='postpage_message'>message</h1>
+            <h1 className='postpage_date'>{props.date}</h1>
+            </div>
+            <div className='postpage_messagelikes'>
+            <h1 className='postpage_likes' >{props.likes} likes</h1>
+            <h1 className='postpage_message'>{props.message}</h1>
+            </div>
         </div>
+
 
         </div>
 
