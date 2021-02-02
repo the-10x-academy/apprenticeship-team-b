fe1b4827678365fd5b52bf5d6d6b55d1a85c8f6e
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-02 21:32:22 +0530

    modified header

---
+++ client/src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import icon from "./icon.png";
+import camera from "./camera.png"
+import "./header.css";
+
+
+
+function Header(){
+    return (
+        
+    <div id = "head">
+        <div id = "insta">
+        <img src = {icon} id = "icon"></img>
+        <text id = "logo">Instaclone</text>
+        </div>
+        <button class = "buttons"><img src = {camera} id = "camera"></img></button>
+    </div>
+    );
+}
+
+export default Header;
