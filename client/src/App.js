58792fdd3a47feee0c97d50b3eff77fe50ba06d6
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-02 21:37:39 +0530

    app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import LandingPage from './Components/HomePage/LandingPage'
+function App(){
+    return(
+        <div>
+            <Header />
+            <LandingPage />
+        </div>
+    );
+}
+export default App;
