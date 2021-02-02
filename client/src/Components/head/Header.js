d50708d85d4e7d69a4f1aefd6737c3b87291261d
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-03 01:18:52 +0530

    head folder

---
+++ client/src/Components/head/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import icon from "./icon.png";
+import camera from "./camera.png"
+import "./header.css";
+import { Link } from "react-router-dom";
+
+
+function Header(){
+    return (
+        
+    <div id = "head">
+        <div id = "insta">
+        <img src = {icon} id = "icon"></img>
+        <text id = "logo">Instaclone</text>
+        </div>
+        <Link to="/posts/create" style={{ textDecoration: "none" }}><button class = "buttons"><img src = {camera} id = "camera"></img></button></Link>
+        
+    </div>
+    );
+}
+
+export default Header;
