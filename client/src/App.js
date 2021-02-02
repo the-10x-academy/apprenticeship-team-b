aad545f009bd36a4a877dfcbdc16a530993a29b0
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-02 23:29:18 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import {
 	Link,
 	Redirect,
 } from "react-router-dom";
+import Homepage from './Homepage'
 import UploadHeader from './UploadHeader'
 import Postlist from './Postlist'
 function App(){
     return(
         <Router>
         <Switch>
+        <Route exact path="/" component={Homepage} />
             <Route exact path ="/posts/create" component = {UploadHeader}/>
             <Route exact path="/posts" component={Postlist} />
         </Switch>
