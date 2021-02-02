963b70bf3c15688dce32a5c997847bd9a371ccef
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-03 01:17:40 +0530

    landing page folder

---
+++ client/src/Components/Landing/Postlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../head/Header'
+import LandingPage from './LandingPage'
+function Postlist(){
+    return(
+        <div>
+            <Header />
+            <LandingPage />
+        </div>
+    );
+}
+export default Postlist;
