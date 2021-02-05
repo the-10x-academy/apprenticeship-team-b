ebc2f0b2b8db91de3aafbf7278160d6c41299e57
Leenesh Sawner <leenesh2015@gmail.com>
2021-02-05 12:15:57 +0530

    multerf

--- models/post.js
+++ models/post.js
@@ -1,13 +1,27 @@
-var mongoose=require('mongoose')
-var Schema=mongoose.Schema
+var mongoose = require("mongoose");
+const multer = require("multer");
+const POST_PATH = "/upload/images";
+var Schema = mongoose.Schema;
 var schema = new Schema({
-    imagePath:{type:String,required:true},
-    name:{type:String,required:true},
-    location:{type:String,required:true},
-    date:{type : Date, default: Date.now},
-    message:{type:String,required:true},
-    likes:{type:Number,required:true,min:0}
-
+	imagePath: { type: String, required: true },
+	name: { type: String, required: true },
+	location: { type: String, required: true },
+	date: { type: Date, default: Date.now },
+	message: { type: String, required: true },
+	likes: { type: Number, required: true, min: 0 },
 });
-const Post=mongoose.model('post',schema)
-module.exports=Post;
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, POST_PATH);
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + "-" + Date.now());
+	},
+});
+//statics
+postSchema.statics.uploadedpost = multer({ storage: storage }).single(
+	"imagePath",
+); // static function
+postSchema.statics.POST_PATH = POST_PATH;
+const Post = mongoose.model("post", schema);
+module.exports = Post;
