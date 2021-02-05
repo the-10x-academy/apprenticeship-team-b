a9bbc13797a5a87456ef30b4f1e1db4daa7d345f
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-05 12:03:01 +0530

    postpage name modified

---
+++ client/src/Components/Post/Post.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './postpage.css';
+import heart from './heart.png'
+import share from './share.png'
+function Post(props){
+    console.log(props)
+    return(
+        <div className='postpage_all'>
+        <div className='postpage_box'>
+
+            <div className='postpage_nameLoc'>
+                <h1 className='postpage_name'>{props.name}</h1>
+                <h1 className='postpage_location'>{props.location}</h1>
+            </div>  
+
+            <div className='postpage_bikediv' style={{ 
+      backgroundImage: `url(${props.imagepath})`}}>
+            </div>
+
+            <div className='postpage_heartshare'>
+            <input type="image" src={heart} alt=' ' className='postpage_heart'/>
+            <input type='image' src={share} alt=' ' className='postpage_share'/>
+            <h1 className='postpage_date'>{props.date}</h1>
+            </div>
+
+            
+            <div className='postpage_messagelikes'>
+            <h1 className='postpage_likes' >{props.likes} likes</h1>
+            <h1 className='postpage_message'>{props.message}</h1>
+            </div>
+        </div>
+
+
+        </div>
+
+    )
+}
+export default Post;
