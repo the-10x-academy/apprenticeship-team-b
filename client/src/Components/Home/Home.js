e0d4c2bb03d90ab75d7718201ff8e2bb634bc577
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-05 11:09:36 +0530

    first page

---
+++ client/src/Components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './homepage.css'
+import logo from './10x.jpeg'
+import { Link } from "react-router-dom";
+function Home(){
+    return(
+        <div className='homepageparent'>
+            <div className='homepageimage'><img className='homepagei' src={logo} alt=''></img>
+            <div className='homepagesecond'>
+                <div>
+                    <center>
+                    <h1 className='homepageteam'>10x team 02</h1>
+                    <div><Link to="/posts" style={{ textDecoration: "none" }}><button className='homepageenter'>Enter</button></Link></div>
+                    
+                    </center>
+                    
+                </div>
+            </div>
+            </div>
+        </div>
+
+    );
+}
+export default Home;
