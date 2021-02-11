01e850810acb0b4f2cedda122e011dbbd1db327d
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-11 16:43:38 +0530

    files separated

--- client/src/Components/Post/Post.js
+++ client/src/Components/Post/Post.js
@@ -1,38 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import Postimage from './Postimage'
+import Postdetails from './Postdetails'
 import './postpage.css';
-import heart from './heart.png'
-import share from './share.png'
 function Post(props){
     console.log(props)
-    const [likes, setLikes] = useState(props.likes);
-    const likesbutton=(e)=>{
-        setLikes(likes+1)
-    }
     return(
         <div className='postpage_all'>
         <div className='postpage_box'>
-
             <div className='postpage_nameLoc'>
                 <h1 className='postpage_name'>{props.name}</h1>
                 <h1 className='postpage_location'>{props.location}</h1>
             </div>  
-            <div className='postpage_bikediv'><img className='post_image'  src = {"http://localhost:9000/" + props.imagepath} /></div>
-            
-            
-            <div className='postpage_heartshare'>
-            <input type="image" src={heart} alt=' ' className='postpage_heart' onClick={likesbutton}/>
-            <input type='image' src={share} alt=' ' className='postpage_share'/>
-            <h1 className='postpage_date'>{props.date}</h1>
-            </div>
-
-            
-            <div className='postpage_messagelikes'>
-            <h1 className='postpage_likes' >{likes} likes</h1>
-            <h1 className='postpage_message'>{props.message}</h1>
-            </div>
+            <Postimage imagepath={props.imagepath} />
+            <Postdetails likes={props.likes} message={props.message} date={props.date} />
         </div>
-
-
         </div>
 
     )
