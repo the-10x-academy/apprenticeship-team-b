ab8c5181382f3f907e2de307ee1bb16ca65eeeba
manchalavaishnavi <manchalavaishnavi07@gmail.com>
2021-02-11 16:41:50 +0530

    post details file separated

---
+++ client/src/Components/Post/Postdetails.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './postpage.css';
+import heart from './heart.png'
+import share from './share.png'
+
+function Postdetails(props){
+    const [likes, setLikes] = useState(props.likes);
+    const [postId, setPostId]=useState("");
+    const likesbutton= async (e)=>{
+        setLikes(likes+1)
+        console.log(likes)
+        console.log('post')
+        const data = new FormData();
+        data.append("likes",likes)
+        data.append("id",props.id)
+        console.log(data)
+        fetch('http://localhost:9000/post/' + props.id, {
+            method: 'PUT',
+            body: data,
+            
+        }).then(res => res.json())
+        .then((data)=>console.log(data));
+    }
+    return (
+        <div>
+            <div className='postpage_nameLoc'>
+                <h1 className='postpage_name'>{props.name}</h1>
+                <h1 className='postpage_location'>{props.location}</h1>
+            </div> 
+            <div className='postpage_heartshare'>
+            <input type="image" src={heart} alt=' ' className='postpage_heart' onClick={likesbutton}/>
+            <input type='image' src={share} alt=' ' className='postpage_share'/>
+            <h1 className='postpage_date'>{props.date}</h1>
+            </div>
+
+            
+            <div className='postpage_messagelikes'>
+            <h1 className='postpage_likes' >{props.likes} likes</h1>
+            <h1 className='postpage_message'>{props.message}</h1>
+            </div>
+        </div>
+    )
+}
+export default Postdetails;
